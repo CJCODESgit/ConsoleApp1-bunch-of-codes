077f68fc92d0cd898c4e8f37ae70741b6ab0879b
John Covenant <ufotjohnamos@gmail.com>
2021-09-22 14:20:58 +0100

    Add files via upload

---
+++ ConsoleApp1/Students.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Students
+    {
+        public string name;
+        public string department;
+        public double gpa;
+    
+
+    public Students(string aName, string aDepartment, double aGpa)
+    {
+        name = aName;
+        department = aDepartment;
+        gpa = aGpa;
+        }
+
+        public bool HasHonors()
+        {
+            if (gpa >= 3.5)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
